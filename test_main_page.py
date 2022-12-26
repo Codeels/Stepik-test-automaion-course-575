e442eb9e93769d61d51406fdaf6b36022ce88dbb
Codeas <ilyas.kodirov@gmail.com>
2022-12-26 21:43:45 +0500

    LoginPage object added to test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,21 +1,23 @@
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
 from pages.base_page import BasePage
-from pages.login_page import *
+from pages.login_page import LoginPage
 
 
-link = "http://selenium1py.pythonanywhere.com/"
+# link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
@@ -40,4 +42,5 @@ def test_should_be_registration_form(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
+
 
